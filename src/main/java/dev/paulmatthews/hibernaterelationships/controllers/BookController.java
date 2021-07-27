91646ca5f0e385cbc6cf71a14fb154d15af0551f
pdmxdd <pdmxdd@gmail.com>
2021-07-27 01:15:29 -0500

    o2m example book has one author, an author can have many books

--- src/main/java/dev/paulmatthews/hibernaterelationships/controllers/BookController.java
+++ src/main/java/dev/paulmatthews/hibernaterelationships/controllers/BookController.java
@@ -2,12 +2,8 @@ package dev.paulmatthews.hibernaterelationships.controllers;
 
 import dev.paulmatthews.hibernaterelationships.dataRepos.AuthorRepository;
 import dev.paulmatthews.hibernaterelationships.dataRepos.BookRepository;
-import dev.paulmatthews.hibernaterelationships.dataRepos.GenreRepository;
-import dev.paulmatthews.hibernaterelationships.dataRepos.ISBNRepository;
 import dev.paulmatthews.hibernaterelationships.models.Author;
 import dev.paulmatthews.hibernaterelationships.models.Book;
-import dev.paulmatthews.hibernaterelationships.models.Genre;
-import dev.paulmatthews.hibernaterelationships.models.ISBN;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,71 +22,6 @@ public class BookController {
     @Autowired
     AuthorRepository authorRepository;
 
-    @Autowired
-    ISBNRepository isbnRepository;
-
-    @Autowired
-    GenreRepository genreRepository;
-
-    @PostMapping(value = "/{bookId}")
-    public String addBookGenreById(@PathVariable int bookId, @RequestParam int genreId, Model model) {
-        // get the book by bookId
-        Optional<Book> maybeBook = bookRepository.findById(bookId);
-
-        // check that the book exists:
-        if(maybeBook.isEmpty()) {
-            return "book-by-id";
-        }
-        // unpack the book
-        Book theBook = maybeBook.get();
-
-        // get the genre by genreId
-        Optional<Genre> maybeGenre = genreRepository.findById(genreId);
-
-        // check that the genre exists:
-        if(maybeGenre.isEmpty()) {
-            return "book-by-id";
-        }
-        // unpack the genre:
-        Genre theGenre = maybeGenre.get();
-
-        // add the genre to the book
-        theBook.addGenre(theGenre);
-
-        // add the book to the genre
-        theGenre.addBook(theBook);
-
-        // save them
-        bookRepository.save(theBook);
-        genreRepository.save(theGenre);
-
-        ArrayList<Genre> allGenres = new ArrayList<>();
-        for(Genre genre : genreRepository.findAll()) {
-            allGenres.add(genre);
-        }
-        model.addAttribute("book", theBook);
-        model.addAttribute("allGenres", allGenres);
-        return "book-by-id";
-    }
-
-    @GetMapping(value = "/{bookId}")
-    public String getBookById(@PathVariable int bookId, Model model) {
-        Optional<Book> maybeBook = bookRepository.findById(bookId);
-        if(maybeBook.isEmpty()) {
-            model.addAttribute("book", "not found");
-            return "book-by-id";
-        }
-        Book foundBook = maybeBook.get();
-        ArrayList<Genre> allGenres = new ArrayList<>();
-        for(Genre genre : genreRepository.findAll()) {
-            allGenres.add(genre);
-        }
-        model.addAttribute("book", foundBook);
-        model.addAttribute("allGenres", allGenres);
-
-        return "book-by-id";
-    }
-
     @GetMapping
     public String getBooks(Model model) {
         ArrayList<Book> bookList = new ArrayList<>();
@@ -101,15 +32,9 @@ public class BookController {
         for(Author author : authorRepository.findAll()) {
             authorList.add(author);
         }
-        ArrayList<ISBN> unregisteredIsbns = new ArrayList<ISBN>();
-        for(ISBN isbn : isbnRepository.findAll()) {
-            if(isbn.getBook() == null) {
-                unregisteredIsbns.add(isbn);
-            }
-        }
+
         model.addAttribute("books", bookList);
         model.addAttribute("authors", authorList);
-        model.addAttribute("unregisteredIsbns", unregisteredIsbns);
         return "books";
     }
 
@@ -118,9 +43,7 @@ public class BookController {
         // we save the book like normal
         bookRepository.save(newBook);
         // we have to update the isbn as well because it needs to track the id of the book.
-        ISBN isbnUpdate = isbnRepository.findById(newBook.getIsbn().getId()).get();
-        isbnUpdate.setBook(newBook);
-        isbnRepository.save(isbnUpdate);
+
         ArrayList<Book> bookList = new ArrayList<>();
         for(Book book : bookRepository.findAll()) {
             bookList.add(book);
@@ -129,15 +52,9 @@ public class BookController {
         for(Author author : authorRepository.findAll()) {
             authorList.add(author);
         }
-        ArrayList<ISBN> unregisteredIsbns = new ArrayList<ISBN>();
-        for(ISBN isbn : isbnRepository.findAll()) {
-            if(isbn.getBook() == null) {
-                unregisteredIsbns.add(isbn);
-            }
-        }
+
         model.addAttribute("books", bookList);
         model.addAttribute("authors", authorList);
-        model.addAttribute("unregisteredIsbns", unregisteredIsbns);
         return "books";
     }
 }
--- src/main/java/dev/paulmatthews/hibernaterelationships/models/Book.java
+++ src/main/java/dev/paulmatthews/hibernaterelationships/models/Book.java
@@ -1,7 +1,6 @@
 package dev.paulmatthews.hibernaterelationships.models;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 public class Book {
@@ -12,32 +11,6 @@ public class Book {
 
     @ManyToOne
     private Author author;
-
-    @OneToOne
-    private ISBN isbn;
-
-    @ManyToMany
-    private List<Genre> genres;
-
-    public List<Genre> getGenres() {
-        return genres;
-    }
-
-    public void addGenre(Genre genre) {
-        genres.add(genre);
-    }
-
-    public void setGenres(List<Genre> genres) {
-        this.genres = genres;
-    }
-
-    public ISBN getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(ISBN isbn) {
-        this.isbn = isbn;
-    }
 
     public Book() {}
 
